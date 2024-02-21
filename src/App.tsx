7f5c4526160d37ee6fbbfbbe3aeb747cc6db0791
KristinaFinck <kristana.finck83@gmail.com>
2024-02-21 18:12:00 +0200

    create project

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    // полезное что-то
+    // обязана вернуть JSX
+    return (
+        <div className="App">
+            This is APP component
+            <Rating/>
+            <Accordion/>
+        </div>
+    );
+}
+
+function Rating() {
+    return (
+        <div>
+            <div>start</div>
+            <div>start</div>
+            <div>start</div>
+            <div>start</div>
+            <div>start</div>
+            <div>start</div>
+        </div>
+    )
+}
+
+function Accordion() {
+    return (
+        <div>
+            <h3> Меню</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
+
+}
+
+export default App;
